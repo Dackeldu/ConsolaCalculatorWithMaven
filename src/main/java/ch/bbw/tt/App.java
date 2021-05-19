6ad27c92ba3adb3e880cc508bc93d8c1b891526d
Timon <dackeldu007@gmail.com>
2021-05-19 11:22:17 +0200

    added project files, changed .gitignore

---
+++ src/main/java/ch/bbw/tt/App.java
@@ -0,0 +1,13 @@
+package ch.bbw.tt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
