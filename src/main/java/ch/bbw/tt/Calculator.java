69f5e329facfd6ceb972c5bdaf81da2d2fda844e
Timon <dackeldu007@gmail.com>
2021-06-29 22:13:15 +0200

    Added functionalit and JUnit-tests

--- src/main/java/ch/bbw/tt/Calculator.java
+++ src/main/java/ch/bbw/tt/Calculator.java
@@ -10,6 +10,8 @@ public class Calculator {
         return value1 - value2;
     }
 
+    public int divide(int value1, int value2) {return  value1 / value2;}
+
     public int getCountOfCalculations() {
         return countOfCalculations;
     }
--- src/test/java/ch/bbw/tt/CalculatorTest.java
+++ src/test/java/ch/bbw/tt/CalculatorTest.java
@@ -33,5 +33,71 @@ public class CalculatorTest {
         assertTrue(testee.summe(-10, -25) == -35);
     }
 
+    @Test
+    public void testGetterUndSetter() {
+        testee.setCountOfCalculations(8);
+        assertTrue(testee.getCountOfCalculations() == 8);
+    }
+
+    @Test(expected=java.lang.AssertionError.class)
+    public void assertionErrorDivision1() {
+        assertTrue(testee.divide(Integer.MAX_VALUE , 3) == Integer.MAX_VALUE);
+    }
+
+    @Test(expected=java.lang.AssertionError.class)
+    public void assertionErrorDivision2() {
+        assertTrue(testee.divide(Integer.MIN_VALUE , 3) == Integer.MAX_VALUE);
+    }
+
+    @Test(expected=java.lang.AssertionError.class)
+    public void assertionErrorSumme1() {
+        assertTrue(testee.summe(Integer.MAX_VALUE , 3) == Integer.MAX_VALUE);
+    }
+
+    @Test(expected=java.lang.AssertionError.class)
+    public void assertionErrorSumme2() {
+        assertTrue(testee.summe(Integer.MIN_VALUE , 3) == Integer.MAX_VALUE);
+    }
+
+    @Test(expected=java.lang.AssertionError.class)
+    public void assertionErrorSubtraction1() {
+        assertTrue(testee.subtraction(Integer.MAX_VALUE , 3) == Integer.MAX_VALUE);
+    }
+
+    @Test(expected=java.lang.AssertionError.class)
+    public void assertionErrorSubtraction2() {
+        assertTrue(testee.subtraction(Integer.MIN_VALUE , 3) == Integer.MAX_VALUE);
+    }
+
+
+    @Test(expected=java.lang.ArithmeticException.class)
+    public void divideZeroError() {
+        assertTrue(testee.divide(8 , 0) == Integer.MAX_VALUE);
+    }
+
+    @Test(expected=java.lang.ArithmeticException.class)
+    public void divideZeroByZero() {
+        assertTrue(testee.divide(0 , 0) == 1);
+    }
+
+    @Test
+    public void divideZeroIsOk() {
+        assertTrue(testee.divide(0 , 912) == 0);
+    }
+
+    @Test
+    public void divideNegativeIsOk() {
+        assertTrue(testee.divide(-110 , 11) == -10);
+    }
+
+    @Test
+    public void divideByNegativeIsOk() {
+        assertTrue(testee.divide(110 , -11) == -10);
+    }
+
+    @Test
+    public void divideNegativeByNegativeIsOk() {
+        assertTrue(testee.divide(-110 , -11) == 10);
+    }
 
 }
